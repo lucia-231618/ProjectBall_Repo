245d30e744f119ab4bbfef65d4265044587b43fe
Ssanbas <94472180+Ssanbas@users.noreply.github.com>
2025-10-27 10:25:38 +0100

    Scene 4 + Script

--- ProyectBall_Proyect/Assets/RAB_Root/Scripts/PlayerController.cs
+++ ProyectBall_Proyect/Assets/RAB_Root/Scripts/PlayerController.cs
@@ -1,117 +1,34 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class NewMonoBehaviourScript : MonoBehaviour
 {
-    [Header("Editor References")]
-    public Rigidbody playerRb; //Referencia al Rigidbody del player
-    public AudioSource playerAudio; //Ref al emisor de sonidos del player
+    [Header("Configuración del jugador")]
+    public float speed = 10f; // Velocidad de movimiento
 
-    [Header("Movement Parameters")]
-    public float speed = 10;
-    public Vector2 moveInput; //Almacén del input de movimiento de los periféricos que usamos para jugar
+    [Header("Configuración del juego")]
+    public float timeLimit = 60f; // Tiempo máximo en segundos
+    private float timeRemaining;  // Tiempo que queda
+    private int totalPolizones;   // Total de polizones en la escena
+    private int foundPolizones = 0; // Cuántos has encontrado
+    private bool gameEnded = false;
 
-    [Header("Jump Parameters")]
-    public float jumpForce = 6;
-    public bool isGrounded = true;
+    [Header("Interfaz UI")]
+    public Text timerText;
+    public Text polizonesText;
+    public Text infoText;
 
-    [Header("Respawn System")]
-    public float fallLimit = -10;
-    public Transform respawnPoint;
+    private Rigidbody rb;
 
-    [Header("Sound Configuration")]
-    public AudioClip[] soundCollection;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
+    {
+ 
+    }
+
+    
+        // Update is called once per frame
+        void Update()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //CinematicMovement();
-        //Respawn por altura
-        if (transform.position.y <= fallLimit)
-        {
-            Respawn();
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        //Update para calcular movimientos físicos
-        PhysicalMovement();
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true; //Devuelve la capacidad de saltar
-        }
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            Respawn();
-        }
-    }
-
-
-    void CinematicMovement()
-    {
-        //Movimiento = (Dirección * velocidad * input)
-        //Necesitais multiplicar el movimiento por Time.deltaTime
-        transform.Translate(Vector3.right * speed * moveInput.x * Time.deltaTime);
-        transform.Translate(Vector3.forward * speed * moveInput.y * Time.deltaTime);
-    }
-
-    void PhysicalMovement()
-    {
-        //Añadir una fuerza al rigidbody = (Dirección * velocidad * input)
-        playerRb.AddForce(Vector3.right * speed * moveInput.x);
-        playerRb.AddForce(Vector3.forward * speed * moveInput.y);
-    }
-
-    void Jump()
-    {
-        playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        PlaySFX(0);
-    }
-
-    void Respawn()
-    {
-        //Sustituir el transform.position del player por el del punto de respawn
-        transform.position = respawnPoint.position;
-        //Resetear el valor de aceleración del rigidbody
-        playerRb.linearVelocity = new Vector3(0,0,0);
-        PlaySFX(2);
-    }
-
-    public void PlaySFX(int soundToPlay)
-    {
-        playerAudio.PlayOneShot(soundCollection[soundToPlay]);
-    }
-
-    #region Input Methods
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
-
-    public void OnJump(InputAction.CallbackContext context) 
-    {
-
-        if (context.performed && isGrounded == true)
-        {
-            isGrounded = false;
-            Jump();
-        }
-    }
-
-
-
-
-    #endregion
 }
